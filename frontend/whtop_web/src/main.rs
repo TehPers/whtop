8ff5e91bf54c4aa591f34f7790d91d66a9373fb5
TehPers <tehperz@gmail.com>
2022-08-30 12:08:53 -0700

    Remove wee_alloc (unmaintained)

--- frontend/whtop_web/src/main.rs
+++ frontend/whtop_web/src/main.rs
@@ -3,9 +3,6 @@ mod contexts;
 mod layers;
 mod routes;
 
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 fn main() {
     yew::start_app::<components::App>();
 }
