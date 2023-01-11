85e7fc3950302b21dffd39a45882c68e096f28bd
TehPers <tehperz@gmail.com>
2023-01-10 21:44:23 -0800

    Add test to CPU route on backend

--- backend/whtop/src/routes/api/system/cpu.rs
+++ backend/whtop/src/routes/api/system/cpu.rs
@@ -35,3 +35,46 @@ fn create_global_cpu_info(cpu: &Cpu) -> GlobalCpuInfo {
         frequency: cpu.frequency(),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{body::Bytes, extract::Extension, http::StatusCode, response::IntoResponse};
+    use std::sync::Arc;
+    use tokio::sync::RwLock;
+    use whtop_common::models::api::GetCpuResponse;
+
+    async fn body_to_bytes<B>(mut body: B) -> Vec<u8>
+    where
+        B: HttpBody<Data = Bytes> + Unpin,
+    {
+        let mut result = Vec::with_capacity(body.size_hint().lower().max(2048) as usize);
+        while let Some(chunk) = body.data().await {
+            let Ok(chunk) = chunk else {
+                unreachable!("error reading chunk from body")
+            };
+
+            result.extend_from_slice(&chunk);
+        }
+
+        result
+    }
+
+    #[tokio::test]
+    async fn test_get_cpu() {
+        // Setup
+        let mut system = System::new_all();
+        system.refresh_all();
+        let system = Arc::new(RwLock::new(system));
+
+        // Execute
+        let response = get_cpu(Extension(system)).await.unwrap();
+
+        // Assert
+        let (parts, body) = response.into_response().into_parts();
+        let body = body_to_bytes(body).await;
+        assert_eq!(parts.status, StatusCode::OK);
+        let body: GetCpuResponse = serde_json::from_slice(&body).unwrap();
+        assert!(body.cpus.len() > 0);
+    }
+}
